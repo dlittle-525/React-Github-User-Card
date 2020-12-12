f591bae845d0cd5b5459306f3104271bce07ed3c
Douglas Little <douglas.a.little@gmail.com>
2020-12-11 20:25:08 -0500

    Add App.css, some of it works

--- learn-to-use-lowercase-filenames-on-cra-projects/src/App.js
+++ learn-to-use-lowercase-filenames-on-cra-projects/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import UserCard from './components/UserCard';
 import './App.css';
 
 class App extends Component {
@@ -11,11 +12,33 @@ constructor() {
   };
 }
 
+componentDidMount() {
+  axios
+    .get('https://api.github.com/users/dlittle-525')
+    .then(res => {this.setState({user: res.data})
+      console.log("user: ", this.state.user);
+    })
+    .catch(err => console.error(err))
+
+  axios
+    .get('https://api.github.com/users/dlittle-525/followers')
+    .then(res => {this.setState({followers: res.data})
+      console.log("followers: ", this.state.followers)
+    let followerArray = this.state.followers.map(follower => follower.login)
+
+    followerArray.forEach(followerLogin => 
+      {axios.get(`http://api.github.com/users/${followerLogin}`)
+      .then(res => {this.setState({followers: res.data})
+    });
+  })
+  });
+}
+
 
   render() {
     return (
       <div className="container">
-        
+        <UserCard user={this.state.user} />
       </div>
     );
   }
---
+++ learn-to-use-lowercase-filenames-on-cra-projects/src/components/UserCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function UserCard(props) {
+    if (!props.user) {
+        return null;
+    }
+    return (
+        <div className='cards'>
+            <div className='card main'>
+                <img src = {props.user['avatar_url']} alt='User Avatar' />
+                <div className='card-info'>
+                    <h3 className='name'>{props.user.name || props.user.login}</h3>
+                    <p className='userName'>{props.user.login}</p>
+                    <p>LocationL {props.user.location || 'Not available'}</p>
+                    <p>Followers: {props.user.followers}</p>
+                    <p>Following: {props.user.following}</p>
+                    <p>Bio: {props.user.bio || 'Not available'}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserCard;
